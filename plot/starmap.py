01ce953b3e06b33930cd0b5e5573d46c04346295
mattsenate <mattsenate@gmail.com>
2012-11-04 13:01:04 -0800

    adds initial functions for starmap plot

---
+++ plot/starmap.py
@@ -0,0 +1,21 @@
+'''
+This is a series of functions to 
+(a) parse some basic starmap data
+(b) plot starmap data
+'''
+
+'''
+Read a file
+Return three dicts
+'''
+def read_coords(file):
+    return file
+
+'''
+Take a square picture object, size of picture in pixels, and dictionary of star values
+Return a plot of the stars using the picture module
+** use add_rect_filled
+'''
+def plot_plain_stars(picture, int, dict):
+    return picture
+
